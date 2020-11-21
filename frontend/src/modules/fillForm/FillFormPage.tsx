e320ad415ed66e121bd2c89407548d00e62ee04b
Wojciech Szmidt <wojciech.szmidt@fortedigital.no>
2020-11-21 16:22:08 +0100

    Styled fillform

--- frontend/src/modules/fillForm/FillFormPage.tsx
+++ frontend/src/modules/fillForm/FillFormPage.tsx
@@ -1,11 +1,15 @@
+import { Col, Row } from 'antd';
 import * as React from 'react';
 import { FillFormStep } from '../shared/fillFormStep/FillFormStep';
 
 export const FillFormPage = () => {
   return (
     <div className="FillFormPage">
-      FillFormPage
-      <FillFormStep />
+      <Row>
+        <Col offset={4} span={16}>
+          <FillFormStep />
+        </Col>
+      </Row>
     </div>
   );
 };
